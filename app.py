7e4b336a1567cc7987486a5d4bf1154eac0aebcc
favourjimi <143393662+favourjimi@users.noreply.github.com>
2025-05-17 14:36:57 -0400

    Update app.py
    edited lat, lon

--- app.py
+++ app.py
@@ -12,13 +12,12 @@ st.markdown("Estimate your solar panel efficiency using real-time weather data."
 
 #--- Inputs ----
 city = st.text_input("Enter the city name:", "New York")
-state = st.text_input("Enter the state name:")
 api_key = st.text_input("Enter your API key:", type="password")
 panel_power = st.number_input("Enter the power of your solar panel (in watts):", min_value=10,max_value=7,value=400, step=0.1)
 efficiency = st.slider("Panel Efficiency (%)",10,25,18)
 
 #--- Get Coordinates from city Name---
-def get_coordinates(city, state, api_key):
+def get_coordinates(city, api_key):
   url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},US&limit=1&appid={api_key}"
   response = requests.get(url)
   data = response.json()
@@ -30,7 +29,7 @@ def get_coordinates(city, state, api_key):
     return None, None
 
 #--- GET WEATHER DATA ----
-def get_weather_data(city, state, api_key):
+def get_weather_data(lat, lon,  api_key):
   url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
   response = requests.get(url)
   data = response.json()
@@ -43,10 +42,10 @@ def calculate_energy_output(panel_power, efficiency, irradiance,num_panels,sunli
   return output
   
 #---- Main Exwcution ---
-if city and state and api_key:
+if city and api_key:
   lat, lon = get_coordinates(city, state, api_key)
   if lat is not None and lon is not None:
-    weather_data = get_weather_data(city, state, api_key)
+    weather_data = get_weather_data(city, api_key)
     if weather_data:
       # Approximate daily sunlight hours and irradiance from cloud cover
       cloud_cover = weather_data['clouds']['all']
